bcaf58be1f9ce18467c9efe58f012cec66462b00
Mahaputra Ilham Awal <51139989+mahaputrailhamawal@users.noreply.github.com>
2020-03-01 21:58:28 +0700

    Add files via upload

---
+++ Python/server1.py
@@ -0,0 +1,34 @@
+import socket, threading
+class ClientThread(threading.Thread):
+    def __init__(self,clientAddress,clientsocket):
+        threading.Thread.__init__(self)
+        self.csocket = clientsocket
+        print ("New connection added: ", clientAddress)
+    def run(self):
+        print ("Connection from : ", clientAddress)
+        #self.csocket.send(bytes("Hi, This is from Server..",'utf-8'))
+        self.csocket.sendall(bytes("Wa'alaikumsalam",'UTF-8'))
+        msg = ''
+        while True:
+            data = self.csocket.recv(2048)
+            msg = data.decode()
+            print ("From Client:", msg)
+            pesan = input()
+            self.csocket.sendall(bytes(pesan,'UTF-8'))
+            if pesan =='bye':
+              break
+            # self.csocket.send(bytes(msg,'UTF-8'))
+        print ("Client at ", clientAddress , " disconnected...")
+        server.close()
+LOCALHOST = "127.0.0.1"
+PORT = 8080
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind((LOCALHOST, PORT))
+print("Server started")
+print("Waiting for client request..")
+while True:
+    server.listen(1)
+    clientsock, clientAddress = server.accept()
+    newthread = ClientThread(clientAddress, clientsock)
+    newthread.start()
